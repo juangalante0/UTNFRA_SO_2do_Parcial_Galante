adc904ef7c4e80ae9244b47006a54a1abb9d3d2f
juangalante0 <juan.galante4@gamil.com>
2024-11-18 22:35:27 +0000

    agrego comandos punto C

--- RTA_Examen_20241116/Punto_C.sh
+++ RTA_Examen_20241116/Punto_C.sh
@@ -2,6 +2,8 @@ sudo usermod -a -G docker juangalante
 nano dockerfile
 nano make-build.sh
 chmod +x make-build.sh
+sudo lvextend -L +300M /dev/vg_datos/lv_docker
+sudo resize2fs /dev/vg_datos/lv_docker
 sh make-build.sh
 nano run.sh
 chmod +x run.sh
